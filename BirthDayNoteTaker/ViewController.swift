8e00db71bd844f93f9e244b9dde929c62edccf72
Tunahan Özataç <tunahan.ozatac@gmail.com>
2019-10-06 15:29:20 +0300

    Initial Commit

---
+++ BirthDayNoteTaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BirthDayNoteTaker
+//
+//  Created by Tunahan Özataç on 6.10.2019.
+//  Copyright © 2019 Tunahan Ozatac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
